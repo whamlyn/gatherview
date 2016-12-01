d874794f39bdc5982b7e66be2e6bd8937c1698df
Wes Hamlyn <wes.hamlyn@gmail.com>
2016-12-01 11:07:35 -0700

    Add files via upload

--- aura_gatherview.py
+++ aura_gatherview.py
@@ -6,11 +6,21 @@
 #   
 #   An application for viewing seismic data stored in SEG-Y format.
 #   
-#       Copyright Wes Hamlyn 2016
-#       
 #       Written by: Wes Hamlyn
 #       Last Mod:   28-Jan-2016
-#
+#   Copyright 2016 Wes Hamlyn
+#   
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#   
+#       http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 #
 # ****************************************************************************
 
@@ -26,11 +36,15 @@ import matplotlib.cm as cm
 from matplotlib.ticker import FormatStrFormatter
 majorFormatter = FormatStrFormatter('%i')
 
-import wx
 import numpy as np
 import os
 
 import auralib as aura
+
+try:
+    import wx
+except ImportError:
+    print("Oops!! Cannot import wxPython, please verify that it is installed")
 
 
 class AuraSEGYView(wx.Frame):
@@ -290,7 +304,7 @@ class AuraSEGYView(wx.Frame):
             t0 = t1 - self.num_disp_traces
             self.sl1.SetValue(t0+1)
         
-        tdata = self.segybuf.read_multi_trace_data(t0, t1)
+        tdata = self.segybuf.read_multi_trace_data_new(t0, t1)
         
         self.tdata = np.array(tdata)
                 
@@ -451,9 +465,9 @@ class AuraSEGYView(wx.Frame):
         """
         
         description = """
-Aura GatherView is a software package for viewing and analysing prestack 
-seismic gather data stored in SEG-Y format.
-"""
+        Aura GatherView is a software package for viewing and analysing prestack 
+        seismic gather data stored in SEG-Y format.
+        """
         
         licence = """None."""
         
